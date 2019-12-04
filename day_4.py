3e1488e0e42e8715ff632f6932fd87d376ce802e
Matthew Crowson <matthew.d.crowson@gmail.com>
2019-12-04 10:30:44 -0500

    slimmer

--- day_4.py
+++ day_4.py
@@ -1,18 +1,11 @@
 
-p1_counter = 0
-p2_counter = 0
+p1_counter, p2_counter = 0, 0
 for n in range(402328, 864247):
     ns = str(n)
     repeats = [ns.count(d) for d in set(ns)]
+    if ns == ''.join(sorted(ns)) and max(repeats) >= 2:
+        p1_counter += 1
+        if 2 in repeats:
+            p2_counter += 1 # part 2 needs a double
 
-    if ns != ''.join(sorted(ns)) or max(repeats) < 2::
-        continue  # Not increasing
-    p1_counter += 1
-    
-    if 2 not in repeats:
-        continue
-
-    p2_counter += 1
-
-print(f"Part 1: {p1_counter}")
-print(f"Part 2: {p2_counter}")
+print(f"Part 1: {p1_counter}. Part 2: {p2_counter}")
