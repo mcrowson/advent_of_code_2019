b78fd38a84d86b7e9730a7468cad9bb4ba2cd2a7
Matthew Crowson <hey@matthewcrowson.com>
2019-12-06 09:56:18 -0500

    day6 done

--- day_6.py
+++ day_6.py
@@ -1,27 +1,19 @@
 from aocd import data
-from anytree import Node, RenderTree
+from anytree import Node, Walker
 
 datan = {(d[:3], d[4:]) for d in data.split('\n')}
-data = 'B)C,C)D,D)E,E)F,B)G,G)H,D)I,E)J,J)K,K)L'
-datan = {(d[:1], d[2:]) for d in data.split(',')}
-datan.add(('COM', 'B'))
-print('hi')
-tree = {}
-for p, c in datan:
-    if p not in tree:
-        tree[p] = Node(p)
-    tree[c] = Node(c, parent=tree[p])
 
-def parents(node, c=0):
-    if not node.parent:
-        return c
-    p = tree[node.parent.name]
-    c += 1
-    return parents(p, c)
+tree = {p: Node(p) for p, c in datan} # Create tree with parent nodes
+for p, c in datan: # Second pass to assign parent nodes 
+    if c in tree:
+        tree[c].parent = tree[p]
+    else:
+        tree[c] = Node(c, parent=tree[p])
 
-total = 0
-for node in tree.values():
-    total += node.depth
+total = sum([n.depth for n in tree.values()])
+print(f"Part 1: {total}")
 
-
-print(total)
+w = Walker()
+path = w.walk(tree['YOU'].parent, tree['SAN'].parent)
+dist = len(path[0]) + len(path[2])
+print(f"Part 2: {dist}")
